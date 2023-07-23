5f8827cdf2133203102fb2180ecaedc8e821d443
Timur Mironenko <timurtop2281337@gmail.com>
2023-07-23 18:54:06 +0300

    Replaced img tags with divs in IntroSection and made this block responsive

--- src/components/IntroSection/IntroSection.tsx
+++ src/components/IntroSection/IntroSection.tsx
@@ -6,8 +6,8 @@ import { ReqInviteBtn } from '../ReqInviteBtn/ReqInviteBtn'
 export const IntroSection = () => {
     return (
         <div className={styles.container}>
-            <img src={backgroundImage} className={styles.backgroundImage} alt='BackgroundImage' />
-            <img src={mockupImage} className={styles.mockupImage} alt='MockupImage' />
+            <div className={styles.backgroundImage} />
+            <div className={styles.mockupImage} />
             <div className={styles.textBlock}>
                 <h1 className={styles.textBlock__header}>Next generation digital banking</h1>
                 <p className={styles.textBlock__text}>Take your financial life online. Your EasyBank account will be a one-stop shop for spending, saving, budgeting, investing, and much more.</p>
